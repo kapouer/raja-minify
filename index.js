7b244b52b80ccfd838858c479b384aa7b3a00105
Jérémy Lal <kapouer@melix.org>
2015-02-10 02:40:19 +0100

    Take 2 - define API compatible with constraints

--- index.js
+++ index.js
@@ -12,140 +12,137 @@ module.exports = function(raja, opts) {
 };
 
 function domAuthorMinify(raja, opts, h, req, res) {
-	h.page.run(domTransform, function(err, maps, cb) {
+	h.page.run(domTransform, function(err, groups, cb) {
 		if (err) return cb(err);
-		build(raja, maps, opts, cb);
+		build(raja, groups, opts, cb);
 	});
 }
 
 function domTransform(done) {
-	function populateMaps(maps, tagName, src, atts, type) {
-		var nodes = Array.prototype.slice.call(document.querySelectorAll(tagName));
+	function renameTo(src, to) {
+		if (src != to) {
+			if (to[0] == '../' || to[0] == './' || to[0] != '/') to = src + '/../' + to;
+		}
+		var split = to.split('.');
+		split.splice(-1, 0, 'min');
+		to = split.join('.');
+		return to;
+	}
+	function getGroups(selector, att, mime) {
+		var groups = [];
+		var nodes = Array.prototype.slice.call(document.querySelectorAll(selector));
+		var group;
 		nodes.forEach(function(node) {
-			var minify = node.hasAttribute('minify');
-			if (!minify) return;
-			var single = false;
-			var dest = node.getAttribute('minify');
-			if (!dest) {
-				single = true;
-				dest = node[src].split('.');
-				dest.push('min', dest.pop());
-				dest = dest.join('.');
+			var single = !node.hasAttribute('to');
+			if (!group || single) {
+				group = {list: []};
+				groups.push(group);
 			}
-			var map = maps[dest];
-			if (!map) {
-				var destNode = document.createElement(tagName);
-				for (var name in atts) {
-					destNode[name] = atts[name];
-				}
-				destNode[src] = dest;
-				node.parentNode.insertBefore(destNode, node);
-				map = maps[dest] = {
-					list:[],
-					url: destNode[src],
-					single: single
-				};
-				for (var name in atts) {
-					map[name] = atts[name];
-				}
-				if (type) map.type = type;
-			}
-			node.parentNode.removeChild(node);
-			if (map.single && map.list.length == 1) {
-				throw new Error("cannot automatically name minified files with the same names", dest);
-			} else {
-				map.list.push(node[src]);
-			}
+			group.list.push(node);
+			if (node.getAttribute('to') || single) group = null;
 		});
+		groups.forEach(function(group) {
+			var list = [];
+			var last = group.list.slice(-1).pop();
+			group.list.forEach(function(node) {
+				if (node[att]) list.push({src: node[att]});
+				else if (node.textContent) list.push({text: node.textContent});
+				if (node != last) {
+					var next = node.nextSibling;
+					while (next && next.nodeType == 3) {
+						next.parentNode.removeChild(next);
+						next = next.nextSibling;
+					}
+					node.parentNode.removeChild(node);
+				} else {
+					node[att] = renameTo(node[att], node.getAttribute('to') || node[att]);
+					group.to = node[att];
+					node[att] = group.to;
+					node.removeAttribute('to');
+				}
+			});
+			group.list = list;
+			delete group.last;
+			group.mime = mime;
+		});
+		return groups;
 	}
-	var maps = {}, err = null;
-	try {
-		populateMaps(maps, 'script', 'src', {type:'text/javascript', charset:'utf-8'});
-		populateMaps(maps, 'link', 'href', {type:'text/css', rel:'stylesheet'});
-	} catch(e) {
-		err = e;
-	}
-	done(err, maps);
+
+	var scripts = getGroups('script', 'src', 'text/javascript');
+	var styles = getGroups('link[rel="stylesheet"]', 'href', 'text/css');
+	done(null, scripts.concat(styles));
 }
 
-function build(raja, maps, opts, cb) {
-	if (Object.keys(maps).length == 0) return cb();
-	var q = queue(1);
-	for (var path in maps) {
-		var map = maps[path];
-		var type = map.type;
-		var batch = {
-			"text/css": batchCss,
-			"text/javascript": batchJs
-		}[type];
-		if (!batch) {
-			console.error("Unsupported type in", map);
-			continue;
-		}
-		var resource = raja.create(map.url);
-		if (map.charset) type += '; charset=' + map.charset;
+function build(raja, groups, opts, cb) {
+	if (groups.length == 0) return cb();
+	var q = queue(3);
+	groups.forEach(function(group) {
+		var resource = raja.create(group.to);
 		resource.headers = {};
-		resource.headers['Content-Type'] = type;
+		resource.headers['Content-Type'] = group.mime;
 		// not so useful - minified files already are declared as dependencies of the authorUrl
 		// if (!resource.parents) resource.parents = {};
 		// resource.parents[h.authorUrl] = true;
-		q.defer(batch, resource, map.list, opts);
-	}
+		if (group.mime == "text/css") {
+			q.defer(batch, resource, group.list, processCss, resultCss, opts);
+		} else if (group.mime == "text/javascript") {
+			q.defer(batch, resource, group.list, processJs, resultJs, opts);
+		}
+	});
 	q.awaitAll(function(err) {
 		// do not pass the list of results
 		cb(err);
 	});
 }
 
-function batchCss(resource, list, opts, cb) {
+function processCss(to, url, data, cur, opts) {
+	if (Buffer.isBuffer(data)) data = data.toString();
+	var parsed = postcss.parse(data, {from: url});
+	postcssUrl({url: "rebase"})(parsed, {from: url, to: to});
+	autoprefixer({ browsers: opts.browsers }).postcss(parsed);
+	csswring.postcss(parsed);
+	if (!cur) cur = parsed;
+	else cur.append(parsed);
+	return cur;
+}
+
+function resultCss(to, cur) {
+	return cur.toResult({to: to}).toString();
+}
+
+function processJs(to, url, data, cur, opts) {
+	if (Buffer.isBuffer(data)) data = data.toString();
+	cur = uglify.parse(data, {filename: url, toplevel: cur});
+	return cur;
+}
+function resultJs(to, cur) {
+	cur.figure_out_scope();
+	cur.compute_char_frequency();
+	cur.mangle_names();
+	var source_map = uglify.SourceMap();
+	return cur.print_to_string({source_map: source_map});
+}
+
+function batch(resource, list, process, result, opts, cb) {
 	var q = queue(1);
 	var cur;
-	list.forEach(function(rurl) {
+	list.forEach(function(obj) {
 		q.defer(function(cb) {
-			resource.load(rurl, function(err, data) {
+			(function(next) {
+				if (obj.src) resource.load(obj.src, next);
+				else if (obj.text) next(null, obj.text);
+				else cb();
+			})(function(err, data) {
 				if (err) return cb(err);
-				if (Buffer.isBuffer(data)) data = data.toString();
-				var parsed = postcss.parse(data, {from: rurl});
-				postcssUrl({url: "rebase"})(parsed, {from: rurl, to: resource.url});
-				autoprefixer({ browsers: opts.browsers }).postcss(parsed);
-				csswring.postcss(parsed);
-				if (!cur) cur = parsed;
-				else cur.append(parsed);
+				cur = process(resource.url, obj.src, data, cur, opts);
 				cb();
-			});
+			})
 		});
 	});
 	q.awaitAll(function(err) {
 		if (err) return cb(err);
 		if (!cur) return cb(new Error("should never happen"));
-		resource.data = cur.toResult({to: resource.url}).toString();
-		resource.save(cb);
-	});
-}
-
-function batchJs(resource, list, opts, cb) {
-	var q = queue(1);
-	var cur = null;
-	list.forEach(function(rurl) {
-		q.defer(function(cb) {
-			resource.load(rurl, function(err, data) {
-				if (err) return cb(err);
-				if (Buffer.isBuffer(data)) data = data.toString();
-				cur = uglify.parse(data, {filename: rurl, toplevel: cur});
-				cb();
-			});
-		});
-	});
-	q.awaitAll(function(err) {
-		if (err) return cb(err);
-		cur.figure_out_scope();
-		cur.compute_char_frequency();
-		cur.mangle_names();
-
-		var source_map = uglify.SourceMap();
-		resource.data = cur.print_to_string({source_map: source_map});
-		// do something with map
-		// var map = source_map.toString();
+		resource.data = result(resource.url, cur);
 		resource.save(cb);
 	});
 }
